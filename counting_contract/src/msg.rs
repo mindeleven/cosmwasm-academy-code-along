f7129d8b2b4e45acf0f9dabcf5210afb5cccb297
mindeleven <mindeleven@posteo.de>
2024-01-27 10:14:51 +0100

    Added a second query message plus some explanatory comments

--- counting_contract/src/msg.rs
+++ counting_contract/src/msg.rs
@@ -4,9 +4,17 @@ use serde::{Deserialize, Serialize};
 /// when the contract is queried, it should be able to create a variety of queries
 /// we typically create query messages as enum types
 /// every single variant is a separate query the contract understands
+/// the message has to derive Deserialize and implement Serialize
+/// so it can be used to send this message from a different contract or tests
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
+    // curly braces here are related to how serde is serializing JSON values
     Value {},
 }
 
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
+#[serde(rename_all = "snake_case")]
+pub struct ValueResp {
+    pub value: u64,
+}
