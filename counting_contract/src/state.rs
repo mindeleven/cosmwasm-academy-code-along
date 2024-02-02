e2f2187d03b15bb17b3f7a49250ba205f7bf25e3
mindeleven <mindeleven@posteo.de>
2024-02-02 09:05:30 +0100

    Updating the state, adding a second Item of type Coin

--- counting_contract/src/state.rs
+++ counting_contract/src/state.rs
@@ -1,6 +1,13 @@
+use cosmwasm_std::Coin;
 use cw_storage_plus::Item;
 
 /// the contract state is defined by creating accessors to the state objects
 /// instead of defining state variables we are creating atoms like Item
-/// these atoms are used to access the values on the blockchain
+/// Item is a type accessing a single object which may exist in the blockchain storage
+/// Item is used to access the values on the blockchain
+/// the string passed to Item on instantiation is part of a key to address the data
+/// Item takes care of serialization and deserialization
 pub const COUNTER: Item<u64> = Item::new("counter");
+/// Coin is a type representing a single native token amount 
+/// it contains a denominator (its unique identifier) and the number of tokens sent
+pub const MINIMAL_DONATION: Item<Coin> = Item::new("minimal_donation");
