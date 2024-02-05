52a193067bb72eec6558ad4d98fb3d1773f696e9
mindeleven <mindeleven@posteo.de>
2024-02-05 11:25:44 +0100

    Replacing previous derives for messages with #[cw_serde]

--- counting_contract/src/msg.rs
+++ counting_contract/src/msg.rs
@@ -1,9 +1,12 @@
 use cosmwasm_std::Coin;
-use schemars::JsonSchema;
-use serde::{Deserialize, Serialize};
+use cosmwasm_schema::cw_serde;
+// use schemars::JsonSchema;
+// use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-#[serde(rename_all = "snake_case")]
+// #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+// #[serde(rename_all = "snake_case")]
+// replacing precious derives with #[cw_serde]
+#[cw_serde]
 pub struct InstantiateMsg {
     #[serde(default)]
     pub counter: u64,
@@ -17,8 +20,10 @@ pub struct InstantiateMsg {
 /// every single variant is a separate query the contract understands
 /// the message has to derive Deserialize and implement Serialize
 /// so it can be used to send this message from a different contract or tests
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
-#[serde(rename_all = "snake_case")]
+// #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+// #[serde(rename_all = "snake_case")]
+// replacing precious derives with #[cw_serde]
+#[cw_serde]
 pub enum QueryMsg {
     // curly braces here are related to how serde is serializing JSON values
     Value {},
@@ -28,21 +33,21 @@ pub enum QueryMsg {
 // creating a new message for the execute entry point
 // define an enum with a single variant per execution message we want to handle
 // the message handler for this is in src/contract.rs
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-#[serde(rename_all = "snake_case")]
+// #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+// #[serde(rename_all = "snake_case")]
+// replacing precious derives with #[cw_serde]
+#[cw_serde]
 pub enum ExecMsg {
     // removing poke and adding donate Msg
     // Poke {},
     Donate {},
     Reset {
-        #[serde(default)]
         counter: u64,
     },
     // execution message variant to send funds
     Withdraw {},
     WithdrawTo {
         receiver: String,
-        #[serde(default)]
         funds: Vec<Coin>,
     },
 }
@@ -50,8 +55,10 @@ pub enum ExecMsg {
 /// second message I created is a response to the Value query
 /// very similar to the Query message but a struct this time
 /// the rename_all attribute is unnecessary but there for consistency
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
-#[serde(rename_all = "snake_case")]
+// #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+// #[serde(rename_all = "snake_case")]
+// replacing precious derives with #[cw_serde]
+#[cw_serde]
 pub struct ValueResp {
     pub value: u64,
 }
